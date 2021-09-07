e28b36584c4eacfd1c78e26e6cef847718ef9330
Sabine Styr <sstyr@oa-m1.msk.21-school.ru>
2021-09-07 17:59:23 +0300

    checked norm

--- include/client.h
+++ include/client.h
@@ -1,11 +1,11 @@
 #ifndef CLIENT_H
-#define CLIENT_H
+# define CLIENT_H
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <signal.h>
-#include "../../printf/includes/ft_printf.h"
-#include <limits.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <signal.h>
+# include "../../printf/includes/ft_printf.h"
+# include <limits.h>
 
 int	ft_str_isnumeric(char *str);
 
--- include/server.h
+++ include/server.h
@@ -1,13 +1,13 @@
 #ifndef SERVER_H
-#define SERVER_H
+# define SERVER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <limits.h>
-#include "../../printf/includes/ft_printf.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <limits.h>
+# include "../../printf/includes/ft_printf.h"
 
 char	*ft_strappc(char *str, char c);
 
